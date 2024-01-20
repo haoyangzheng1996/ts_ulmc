86ee0ada1b41e58ea384c6f9f0cc9dcd97ac1929
Haoyang Zheng <38525155+haoyangzheng1996@users.noreply.github.com>
2024-01-19 21:51:51 -0500

    Delete main.sh

--- main.sh
+++
@@ -1,6 +0,0 @@
-clear
-
-conda activate thompson
-
-python underdamp_langevin.py --n_round 200 --n_arm 10 --dim 2 --batch_size 10 --seed 2403
-# python over_langevin.py --n_round 200 --n_arm 10 --dim 2 --batch_size 10 --seed 2403
